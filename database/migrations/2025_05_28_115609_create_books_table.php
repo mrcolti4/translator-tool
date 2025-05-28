0f0f51f7838c40ead33dc45aac34ea00cdda1659
mrcolti4 <kbulygin456@gmail.com>
2025-05-28 15:04:57 +0300

    wip (database): create migrations for Book and Page

---
+++ database/migrations/2025_05_28_115609_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private const string TABLE_NAME = Book::TABLE_NAME;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE_NAME, function (Blueprint $table) {
+            $table->id();
+            $table->string('author');
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('poster')->nullable();
+            $table->string('release_year');
+            $table->string('language');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE_NAME);
+    }
+};
